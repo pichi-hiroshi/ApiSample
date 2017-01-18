57bf4480987c801e6e99c276640e35e2c50e1300
hiroshi_takahashi <pichi.hiroshi@gmail.com>
2017-01-18 02:19:21 +0000

    dive14_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resources :blogs, only: [:index] #この行を追記
-  resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
   
   root 'top#index'
   
